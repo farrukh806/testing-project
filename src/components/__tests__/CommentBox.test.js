ec3d165e381ab8d25fdeb46e0c90e059856d6b2b
Farrukh <farrukhjamal806@gmail.com>
2021-09-16 17:57:08 +0530

    Comment adding feature tested

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -5,7 +5,7 @@ import CommentBox from '../CommentBox';
 let wrapped;
 
 beforeEach(() => {
-	// Mount function mounts the actual component
+	// Mount function mounts the actual component ==> (Full JS DOM render)
 	wrapped = mount(<CommentBox />);
 });
 
@@ -17,4 +17,18 @@ afterEach(() => {
 it('has a textarea and a button', () => {
 	expect(wrapped.find('textarea').length).toEqual(1);
 	expect(wrapped.find('button').length).toEqual(1);
+});
+
+it('has a textarea that users can type in', () => {
+	wrapped
+		.find('textarea')
+		.simulate('change', { target: { value: 'New comment' } });
+
+	// As we know setState method is async so we have to
+	// force the component to rerender itself using update method
+	// provided by Enzyme so that we dont have to wait for auto rendering process
+	// of the component
+	wrapped.update();
+
+	expect(wrapped.find('textarea').prop('value')).toEqual('New comment');
 });
