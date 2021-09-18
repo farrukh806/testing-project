c0dd9dade9d74826852333ea63a03d30bc9364f6
Farrukh <farrukhjamal806@gmail.com>
2021-09-18 19:12:48 +0530

    Tests written for network request

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -18,9 +18,9 @@ afterEach(() => {
 	wrapped.unmount();
 });
 
-it('has a textarea and a button', () => {
+it('has a textarea and two buttons', () => {
 	expect(wrapped.find('textarea').length).toEqual(1);
-	expect(wrapped.find('button').length).toEqual(1);
+	expect(wrapped.find('button').length).toEqual(2);
 });
 
 describe('the textarea', () => {
